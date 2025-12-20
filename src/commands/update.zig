cff7952d44186ad125e45e7b81aabbb5599d9bf0
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-12-20 01:35:54 +0100

    feat(update): implement online update checking and installation
    Add functionality to check for updates by fetching the latest version from GitHub,
    compare with the current version, and automatically install updates using a remote
    install script. Enhanced the animation to display installation progress and
    improved error handling for network operations.

--- src/commands/update.zig
+++ src/commands/update.zig
@@ -6,28 +6,51 @@ const std = @import("std");
 const terminal = ziglet.utils.terminal;
 const Color = terminal.Color;
 const printColored = terminal.printColored;
+const builtin = @import("builtin");
+
+const InstallationInfo = struct {
+    installing: bool,
+    version: []const u8,
+};
 
 pub fn updateCommand(ctx: CommandContext) !void {
     const allocator = ctx.allocator;
     var checking = Atomic(bool).init(true);
 
-    const animation_thread = try Thread.spawn(.{}, animate, .{&checking});
-    const check_thread = try Thread.spawn(.{}, checkUpdate, .{ &checking, allocator });
+    const installation_info = allocator.create(InstallationInfo) catch unreachable;
+
+    installation_info.* = .{
+        .installing = false,
+        .version = "",
+    };
+
+    var installing = Atomic(*InstallationInfo).init(installation_info);
+
+    const animation_thread = try Thread.spawn(.{}, animate, .{ &checking, &installing });
+    const check_thread = try Thread.spawn(.{}, checkUpdate, .{ &checking, allocator, &installing });
 
     check_thread.join();
+
+    if (installing.load(.acquire).installing) {
+        const install_thread = try Thread.spawn(.{}, install, .{ allocator, &installing });
+        install_thread.join();
+    }
+
+    checking.store(false, .release);
     animation_thread.join();
 
-    // this should be final print
+    defer allocator.destroy(installation_info);
 }
 
-fn animate(is_checking: *Atomic(bool)) void {
+fn animate(is_checking: *Atomic(bool), is_installing: *Atomic(*InstallationInfo)) void {
     const frames = [_][]const u8{ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" };
     var frame_index: usize = 0;
 
-    while (is_checking.load(.acquire)) {
-        std.debug.print("\r{s}{s} Checking for updates...{s}", .{
+    while (is_checking.load(.acquire) or is_installing.load(.acquire).installing) {
+        std.debug.print("\r{s}{s} {s}{s}", .{
             Color.ansiCode(.white),
             frames[frame_index % frames.len],
+            if (is_installing.load(.acquire).installing) "Installing update..." else "Checking for updates...",
             Color.ansiCode(.reset),
         });
         std.Thread.sleep(100 * std.time.ns_per_ms);
@@ -35,29 +58,35 @@ fn animate(is_checking: *Atomic(bool)) void {
     }
 }
 
-fn checkUpdate(is_checking: *Atomic(bool), allocator: std.mem.Allocator) void {
+fn checkUpdate(is_checking: *Atomic(bool), allocator: std.mem.Allocator, is_installing: *Atomic(*InstallationInfo)) void {
+    const values_to_strip = std.ascii.whitespace ++ "\x1b[0m" ++ "\x1b[37m";
 
-    // for now check the version in here
-    var i: usize = 0;
-    while (i < 1000_000_000) {
-        i += 1;
+    var child_process = std.process.Child.init(&.{ "curl", "-sL", "https://raw.githubusercontent.com/Kingrashy12/zio/main/version" }, allocator);
+    child_process.stdout_behavior = .Pipe;
+
+    child_process.spawn() catch |err| {
+        printColored(.red, "Unable to spawn child process:{s}\n", .{@errorName(err)});
+        return;
+    };
+
+    var buffer: [50]u8 = undefined;
+
+    var first_byte_read: usize = 0;
+
+    if (child_process.stdout) |out| {
+        const len = out.readAll(&buffer) catch |err| {
+            printColored(.red, "Unable to read child process output:{s}\n", .{@errorName(err)});
+            return;
+        };
+        first_byte_read = len;
     }
 
-    is_checking.store(false, .release);
-
-    const version_file = std.fs.cwd().openFile("version", .{}) catch |err| {
-        printColored(.red, "Unable to open file:{s}\n", .{@errorName(err)});
+    _ = child_process.wait() catch {
+        printColored(.red, "Unable to wait for child process\n", .{});
         return;
     };
 
-    var buffer: [6]u8 = undefined;
-
-    _ = version_file.readAll(&buffer) catch |err| {
-        printColored(.red, "Unable to read file:{s}\n", .{@errorName(err)});
-        return;
-    };
-
-    const version = buffer[0..buffer.len];
+    const version = std.mem.trim(u8, buffer[0..first_byte_read], &std.ascii.whitespace);
 
     var child = std.process.Child.init(&.{ "zio", "-V" }, allocator);
     child.stdout_behavior = .Pipe;
@@ -84,8 +113,6 @@ fn checkUpdate(is_checking: *Atomic(bool), allocator: std.mem.Allocator) void {
         return;
     };
 
-    const values_to_strip = std.ascii.whitespace ++ "\x1b[0m" ++ "\x1b[37m";
-
     const current_version = std.mem.trim(u8, current_buffer[0..byte_read], values_to_strip);
 
     const is_updated = std.mem.eql(u8, version, current_version);
@@ -93,18 +120,40 @@ fn checkUpdate(is_checking: *Atomic(bool), allocator: std.mem.Allocator) void {
     if (is_updated) {
         printColored(.magenta, "\r🤗 zio is up to date!    \n", .{});
     } else {
-        std.debug.print("\r{s}✓ Found update: {s}   {s}\n", .{
-            Color.ansiCode(.green),
-            version,
+        std.debug.print("\r{s}✨ New update available!   {s}\n", .{
+            Color.ansiCode(.yellow),
             Color.ansiCode(.reset),
         });
+
+        is_installing.load(.seq_cst).installing = true;
+        is_installing.load(.seq_cst).version = allocator.dupe(u8, version) catch unreachable;
     }
+
+    is_checking.store(false, .release);
 }
 
-fn install(installing: *Atomic(bool)) void {
-    var i: usize = 0;
-    while (i < 1000_000_000) {
-        i += 1;
-    }
-    installing.store(false, .release);
+fn install(allocator: std.mem.Allocator, installing: *Atomic(*InstallationInfo)) void {
+    if (!installing.load(.acquire).installing) return;
+
+    const url = if (builtin.os.tag == .windows) "https://raw.githubusercontent.com/Kingrashy12/zio/main/install.bash | bash" else "https://raw.githubusercontent.com/Kingrashy12/zio/main/install.bash | sudo bash";
+
+    var child = std.process.Child.init(&.{ "curl", "-sL", url }, allocator);
+    child.stdout_behavior = .Pipe;
+    child.stderr_behavior = .Pipe;
+
+    child.spawn() catch |err| {
+        printColored(.red, "Unable to spawn child process:{s}\n", .{@errorName(err)});
+        return;
+    };
+
+    _ = child.wait() catch {
+        printColored(.red, "Unable to wait for child process\n", .{});
+        return;
+    };
+
+    installing.load(.seq_cst).installing = false;
+
+    printColored(.green, "\r✓ Update installed '{s}'\n", .{installing.load(.acquire).version});
+
+    allocator.free(installing.load(.seq_cst).version);
 }
---
+++ update.zig
@@ -0,0 +1,238 @@
+// const ziglet = @import("ziglet");
+// const CommandContext = ziglet.CommandContext;
+// const Atomic = std.atomic.Value;
+// const Thread = std.Thread;
+// const std = @import("std");
+// const terminal = ziglet.utils.terminal;
+// const Color = terminal.Color;
+// const printColored = terminal.printColored;
+
+// const InstallationInfo = struct {
+//     installing: bool,
+//     version: []const u8,
+// };
+
+// const TuiEvent = union(enum) {
+//     Checking,
+//     FoundUpdate,
+//     Installing,
+//     Installed,
+//     UpToDate,
+//     Done,
+// };
+
+// const InstallEvent = enum {
+//     Start,
+//     Done,
+// };
+
+// fn EventChannel(comptime Event: type) type {
+//     return struct {
+//         mutex: Thread.Mutex = .{},
+//         cond: Thread.Condition = .{},
+//         queue: std.ArrayList(Event),
+//         allocator: std.mem.Allocator,
+
+//         const Self = @This();
+
+//         pub fn init(allocator: std.mem.Allocator) Self {
+//             return .{
+//                 .queue = .empty,
+//                 .allocator = allocator,
+//             };
+//         }
+
+//         pub fn deinit(self: *Self) void {
+//             self.queue.deinit(self.allocator);
+//         }
+
+//         pub fn send(self: *Self, event: Event) void {
+//             self.mutex.lock();
+//             defer self.mutex.unlock();
+
+//             self.queue.append(self.allocator, event) catch unreachable;
+//             self.cond.signal();
+//         }
+
+//         pub fn recv(self: *Self) Event {
+//             self.mutex.lock();
+//             defer self.mutex.unlock();
+
+//             while (self.queue.items.len == 0) {
+//                 self.cond.wait(&self.mutex);
+//             }
+
+//             return self.queue.orderedRemove(0);
+//         }
+
+//         pub fn tryRecv(self: *Self) ?Event {
+//             self.mutex.lock();
+//             defer self.mutex.unlock();
+
+//             if (self.queue.items.len == 0) return null;
+//             return self.queue.orderedRemove(0);
+//         }
+//     };
+// }
+
+// pub fn updateCommand(ctx: CommandContext) !void {
+//     const allocator = ctx.allocator;
+//     // var checking = Atomic(bool).init(true);
+
+//     var tui = EventChannel(TuiEvent).init(allocator);
+//     var install_channel = EventChannel(InstallEvent).init(allocator);
+
+//     defer tui.deinit();
+//     defer install_channel.deinit();
+
+//     const animator = try std.Thread.spawn(.{}, animate, .{&tui});
+//     const checker = try std.Thread.spawn(.{}, checkUpdate, .{ allocator, &tui, &install_channel });
+//     const installer = try std.Thread.spawn(.{}, install, .{ allocator, &tui, &install_channel });
+
+//     checker.join();
+//     installer.join();
+
+//     tui.send(.Done);
+//     animator.join();
+// }
+
+// fn animate(channel: *EventChannel(TuiEvent)) void {
+//     const frames = [_][]const u8{ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" };
+//     var frame_index: usize = 0;
+
+//     var text: []const u8 = "Checking for updates...";
+//     var text_color: []const u8 = Color.ansiCode(.white);
+//     var running = true;
+
+//     while (running) {
+//         while (channel.tryRecv()) |event| {
+//             switch (event) {
+//                 .Checking => {
+//                     text = "Checking for updates...";
+//                     text_color = Color.ansiCode(.white);
+//                 },
+//                 .Installing => {
+//                     text = "Installing update...";
+//                     text_color = Color.ansiCode(.white);
+//                 },
+//                 .Installed => {
+//                     text = "✓ Update installed";
+//                     text_color = Color.ansiCode(.green);
+//                 },
+//                 .UpToDate => {
+//                     text = "🤗 zio is up to date!";
+//                     text_color = Color.ansiCode(.green);
+//                 },
+//                 .FoundUpdate => {
+//                     text = "New update available!";
+//                     text_color = Color.ansiCode(.yellow);
+//                 },
+//                 .Done => running = false,
+//                 // else => {},
+//             }
+//         }
+
+//         std.debug.print("\r{s}{s} {s}{s}", .{
+//             text_color,
+//             frames[frame_index % frames.len],
+//             text,
+//             Color.ansiCode(.reset),
+//         });
+
+//         frame_index += 1;
+//         std.Thread.sleep(100 * std.time.ns_per_ms);
+//     }
+//     std.debug.print("\n", .{});
+// }
+
+// fn checkUpdate(
+//     allocator: std.mem.Allocator,
+//     tui: *EventChannel(TuiEvent),
+//     install_channel: *EventChannel(InstallEvent),
+// ) void {
+//     tui.send(.Checking);
+
+//     const values_to_strip = std.ascii.whitespace ++ "\x1b[0m" ++ "\x1b[37m";
+
+//     var child_process = std.process.Child.init(&.{ "curl", "-sL", "https://raw.githubusercontent.com/Kingrashy12/zio/main/version" }, allocator);
+//     child_process.stdout_behavior = .Pipe;
+
+//     child_process.spawn() catch |err| {
+//         printColored(.red, "Unable to spawn child process:{s}\n", .{@errorName(err)});
+//         return;
+//     };
+
+//     var buffer: [50]u8 = undefined;
+
+//     var first_byte_read: usize = 0;
+
+//     if (child_process.stdout) |out| {
+//         const len = out.readAll(&buffer) catch |err| {
+//             printColored(.red, "Unable to read child process output:{s}\n", .{@errorName(err)});
+//             return;
+//         };
+//         first_byte_read = len;
+//     }
+
+//     _ = child_process.wait() catch {
+//         printColored(.red, "Unable to wait for child process\n", .{});
+//         return;
+//     };
+
+//     const remote_version = std.mem.trim(u8, buffer[0..first_byte_read], &std.ascii.whitespace);
+
+//     var child = std.process.Child.init(&.{ "zio", "-V" }, allocator);
+//     child.stdout_behavior = .Pipe;
+
+//     child.spawn() catch |err| {
+//         printColored(.red, "Unable to spawn child process:{s}\n", .{@errorName(err)});
+//         return;
+//     };
+
+//     var current_buffer: [50]u8 = undefined;
+
+//     var byte_read: usize = 0;
+
+//     if (child.stdout) |out| {
+//         const len = out.readAll(&current_buffer) catch |err| {
+//             printColored(.red, "Unable to read child process output:{s}\n", .{@errorName(err)});
+//             return;
+//         };
+//         byte_read = len;
+//     }
+
+//     _ = child.wait() catch {
+//         printColored(.red, "Unable to wait for child process\n", .{});
+//         return;
+//     };
+
+//     const current_version = std.mem.trim(u8, current_buffer[0..byte_read], values_to_strip);
+
+//     const is_updated = std.mem.eql(u8, remote_version, current_version);
+
+//     if (is_updated) {
+//         tui.send(.UpToDate);
+//         return;
+//     }
+
+//     tui.send(.FoundUpdate);
+//     tui.send(.Installing);
+
+//     install_channel.send(.Start);
+// }
+
+// fn install(allocator: std.mem.Allocator, tui: *EventChannel(TuiEvent), install_channel: *EventChannel(InstallEvent)) void {
+//     _ = allocator;
+//     while (true) {
+//         switch (install_channel.recv()) {
+//             .Start => {},
+//             .Done => return,
+//         }
+//     }
+
+//     // simulate install work
+//     var i: usize = 0;
+//     while (i < 600_000_000) : (i += 1) {}
+
+//     tui.send(.Installed);
+// }
